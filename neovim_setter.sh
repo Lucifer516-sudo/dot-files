dc952f2d0506d2142759b744ed151b60c86194c5
Lucifer516-sudo <73592721+Lucifer516-sudo@users.noreply.github.com>
2022-07-10 20:11:42 +0530

    Update neovim_setter.sh

--- neovim_setter.sh
+++ neovim_setter.sh
@@ -7,7 +7,7 @@ curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.config/nvim/bundle/
 git clone -b release https://github.com/neoclide/coc.nvim.git
 git clone https://github.com/preservim/nerdtree.git
-git clone git://github.com/jiangmiao/auto-pairs.git
+git clone https://github.com/jiangmiao/auto-pairs.git
 git clone https://github.com/vim-airline/vim-airline.git
 git clone https://github.com/terryma/vim-multiple-cursors.git
 
